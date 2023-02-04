6fe81ef3764a1b1aeff36a3483b257d82c3e3fa0
Dmitry <badl1970@mail.ru>
2023-02-04 17:45:57 +0300

    delete some spaces

--- src/App.java
+++ src/App.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         int seconds = requestNumber();
-        checkTime(seconds);
+        checkTime(seconds);    
     }
-
     static void checkTime(int sec) {
-        int hours = sec / 3600;
-        int minutes = sec % 3600 / 60;
-        int leftSeconds = sec % 60;
+        int hours = sec/3600;
+        int minutes = sec%3600/60;
+        int leftSeconds = sec%60;
         System.out.printf("Прошло:" + "\n" + "полных часов с начала суток - " + hours + "\n" + "полных минут" +
                 " с начала очередного часа - " + minutes + "\n" + "полных секунд с начала очередной минуты - " + leftSeconds);
     }
-
     static int requestNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число:");
-        return scanner.nextInt();
-    }
+        return scanner.nextInt();       
+    }   
 }
