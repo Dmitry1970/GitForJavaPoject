b1eac0f26dc357d6b23b12b602af3face1efe54c
Dmitry <badl1970@mail.ru>
2023-02-04 19:18:57 +0300

    Revert "made some corrections" revert last commit
    This reverts commit 1f57b10a2c5ac07a6473a5e50c8d09142b4a70bf.

--- src/App.java
+++ src/App.java
@@ -6,11 +6,11 @@ public class App {
         checkTime(seconds);    
     }
     static void checkTime(int sec) {
-        int h = sec / 3600;
-        int m = sec % 3600/60;        
-        int leftSec = sec % 60;       
-        System.out.printf("Прошло:" + "\n" + "полных часов с начала суток - " + h + "\n" + "полных минут" +
-                " с начала очередного часа - " + m + "\n" + "полных секунд с начала очередной минуты - " + leftSec);
+        int hours = sec / 3600;
+        int minutes = sec % 3600/60;        
+        int leftSeconds = sec % 60;       
+        System.out.printf("Прошло:" + "\n" + "полных часов с начала суток - " + hours + "\n" + "полных минут" +
+                " с начала очередного часа - " + minutes + "\n" + "полных секунд с начала очередной минуты - " + leftSeconds);
     }
     static int requestNumber() {
         Scanner sc = new Scanner(System.in);
