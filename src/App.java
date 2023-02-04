5b62674d5b166e6f37e1ba22ec48cb9ca3be39f6
Dmitry <badl1970@mail.ru>
2023-02-04 19:06:24 +0300

    deleted some lines

--- src/App.java
+++ src/App.java
@@ -7,17 +7,15 @@ public class App {
     }
     static void checkTime(int sec) {
         int hours = sec / 3600;
-        int minutes = sec % 3600/60;
-        System.out.println(minutes);
-        int leftSeconds = sec % 60;
-        System.out.println(leftSeconds);
+        int minutes = sec % 3600/60;        
+        int leftSeconds = sec % 60;       
         System.out.printf("Прошло:" + "\n" + "полных часов с начала суток - " + hours + "\n" + "полных минут" +
                 " с начала очередного часа - " + minutes + "\n" + "полных секунд с начала очередной минуты - " + leftSeconds);
     }
     static int requestNumber() {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Введите число: ");
-        return scanner.nextInt();
+        return sc.nextInt();
      
     }   
 }
